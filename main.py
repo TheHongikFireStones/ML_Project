d96554493a72327d6a74d5e71c85dc087697add7
qjvk2880 <qjvk2880@gmail.com>
2024-05-12 22:40:48 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import dlib
+import cv2
+import matplotlib.pyplot as plt
+
+# 얼굴 검출기와 랜드마크 검출기 초기화
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# 이미지 로드
+image = cv2.imread("asian_image/1.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# 얼굴 검출
+faces = detector(gray)
+
+for face in faces:
+    x1, y1, x2, y2 = face.left(), face.top(), face.right(), face.bottom()
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+    # 랜드마크 검출
+    landmarks = predictor(gray, face)
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        cv2.circle(image, (x, y), 4, (255, 0, 0), -1)
+
+# BGR 이미지를 RGB로 변환
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# matplotlib를 사용해 이미지 표시
+plt.figure(figsize=(10, 10))
+plt.imshow(image_rgb)
+plt.axis("off")  # 축을 표시하지 않음
+plt.show()
