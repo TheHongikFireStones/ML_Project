b294cb768d4773803c7a5881b82d14a7cd260668
yunhacandy <yunhacandy@gmail.com>
2024-05-24 18:32:24 +0900

    feat: 눈과 눈썹 사이 거리 구하는 함수 추가

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import cv2
 import matplotlib.pyplot as plt
 
 # 눈 크기 측정
-
 LEFT_EYE_LEFT_IDX = 36
 LEFT_EYE_RIGHT_IDX = 39
 
 RIGHT_EYE_LEFT_IDX = 42
-RIGHT_EYE_RIGHT_IDX = 44
+RIGHT_EYE_RIGHT_IDX = 45
 
 def get_left_eye_width(face_left, face_right, landmarks):
     return (landmarks.part(LEFT_EYE_RIGHT_IDX).x - landmarks.part(LEFT_EYE_LEFT_IDX).x) / (face_right - face_left)
@@ -22,15 +21,42 @@ def get_eye_width(face_left, face_right, landmarks):
     average_width = (left_eye_width + right_eye_width) / 2
     return average_width
 
+# 눈과 눈썹 사이 거리 측정
+LEFT_EYEBROW = 19
+LEFT_EYE_IDX1 = 37
+LEFT_EYE_IDX2 = 38
+
+RIGHT_EYEBROW = 24
+RIGHT_EYE_IDX1 = 43
+RIGHT_EYE_IDX2 = 44
+
+def get_left_eyebrow_distance(face_top, face_bottom, landmarks):
+    distance1 = (landmarks.part(LEFT_EYEBROW).y - landmarks.part(LEFT_EYE_IDX1).y) / (face_top - face_bottom)
+    distance2 = (landmarks.part(LEFT_EYEBROW).y - landmarks.part(LEFT_EYE_IDX2).y) / (face_top - face_bottom)
+    return min(distance1, distance2)
+
+def get_right_eyebrow_distance(face_top, face_bottom, landmarks):
+    distance1 = (landmarks.part(RIGHT_EYEBROW).y - landmarks.part(RIGHT_EYE_IDX1).y) / (face_top - face_bottom)
+    distance2 = (landmarks.part(RIGHT_EYEBROW).y - landmarks.part(RIGHT_EYE_IDX2).y) / (face_top - face_bottom)
+    return min(distance1, distance2)
+
+def get_eyebrow_distance(face_top, face_bottom, landmarks):
+    left_eyebrow = get_left_eyebrow_distance(face_top, face_bottom, landmarks)
+    right_eyebrow = get_right_eyebrow_distance(face_top, face_bottom, landmarks)
+    average_eyebrow_width = (left_eyebrow + right_eyebrow) / 2
+    return average_eyebrow_width
+
+# google colab용 코드
+from google.colab import drive
+drive.mount('/content/drive')
+
 # 얼굴 검출기와 랜드마크 검출기 초기화
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-from google.colab import drive
-drive.mount('/content/drive')
-
 # 이미지 로드
-image = cv2.imread("asian_image/1.png")
+file_identifier = ''
+image = cv2.imread("/content/drive/MyDrive/ML_Project_/Oscar Isaac/Oscar Isaac 10.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # 얼굴 검출
@@ -43,6 +69,8 @@ for face in faces:
     # 랜드마크 검출
     landmarks = predictor(gray, face)
     print(get_eye_width(x1, x2, landmarks))
+    print(get_eyebrow_distance(y1, y2, landmarks))
+
     for n in range(0, 68):
         x = landmarks.part(n).x
         y = landmarks.part(n).y
